7bc850eb424ab266defd8a6c0b576c43758bb778
ScriptedButton <coleb@DESKTOP-3ORA1J7>
2019-04-06 13:37:18 -0400

    Resized view bio button.

--- iPass/Form1.Designer.cs
+++ iPass/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // 
             this.button2.Location = new System.Drawing.Point(12, 12);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(258, 23);
+            this.button2.Size = new System.Drawing.Size(592, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "View Bio";
             this.button2.UseVisualStyleBackColor = true;
